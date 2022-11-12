12ac25c299fb379e8e116ed84a2ef48a4a15826a
π2019059 <72515596+xar1sgeovlacp2019059@users.noreply.github.com>
2022-11-12 18:09:39 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
   name='FCBF',
-  version='1.0'
-  author='shiralkarprashant'
+  version='1.0',
+  author='shiralkarprashant',
   packages=find_packages(),
 )
